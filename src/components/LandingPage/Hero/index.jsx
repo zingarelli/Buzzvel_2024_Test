22025d9e0b05b3292c0e24aab2a630f9fd04e250
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2024-10-25 12:48:43 -0300

    add Hero section (mobile)

--- src/components/LandingPage/Hero/index.jsx
+++ src/components/LandingPage/Hero/index.jsx
@@ -1,7 +1,36 @@
+import styles from './Hero.module.css';
+import workerSmall from '../../../assets/worker-mobile.png';
+import worker from '../../../assets/worker.png';
+import { testimonials } from '../../../mock/testimonials';
 import Logo from "../../Logo";
+import QuoteButton from '../../QuoteButton';
+import Testimonial from '../../Testimonial';
+import MenuButton from '../../MenuButton';
 
+/**
+ * Renders the Hero section of the landing page.
+ */
 export default function Hero() {
-  return (
-    <Logo />
-  )
+    return (
+        <header>
+            <div className={styles.navWrapper}>
+                <nav className={styles.nav}>
+                    <Logo />
+                    <MenuButton className={styles.nav__button} />
+                    <picture className={styles.nav__bg}>
+                        <source media="(max-width: 600px)" srcSet={workerSmall} />
+                        <img src={worker} alt="a worker installing solar panels" />
+                    </picture>
+                </nav>
+            </div>
+            <div className={styles.content}>
+                <section className={styles.main}>
+                    <h1 className={styles.title}>Get the Sun to Power Your Home</h1>
+                    <p className={styles.text}>Viverra viverra nibh enim et aliquam, enim. Tempor, sit mus viverra orci dui consequat turpis scelerisque.</p>
+                    <QuoteButton />
+                </section>
+                <Testimonial content={testimonials[0]} highlight />
+            </div>
+        </header>
+    );
 }
