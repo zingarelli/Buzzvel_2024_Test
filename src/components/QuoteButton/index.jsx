c44a33e8b1caf6e47539a1eb728fa21db0e7a9ed
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2024-10-25 10:16:58 -0300

    add QuoteButton component

---
+++ src/components/QuoteButton/index.jsx
@@ -0,0 +1,18 @@
+import styles from './QuoteButton.module.css';
+
+/**
+ * For now this renders a button with the text "Request a quote".
+ * We could change it later to use other props and children to 
+ * make it more generic.
+ * @param {string} theme - change the color (purple | orange). 
+ * Default is purple.
+ */
+export default function QuoteButton({ theme = 'purple' }) {
+    const classTheme = theme === 'purple' ? 'purpleTheme' : 'orangeTheme';
+    console.log(styles[classTheme])
+    return (
+        <button className={`${styles.button} ${styles[classTheme]}`}>
+            Request a Quote &rarr;
+        </button>
+    )
+}
